f4d7bd8f312c769690f0c3ff62c0eaa34a161027
Jaehyeon <xhwogusxh@gmail.com>
2024-07-22 02:22:24 +0900

    3.login 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
 import ChatHeader from "@/components/ChatHeader";
 import { Button } from "@/components/ui/button";
+import InitUser from "@/lib/store/InitUser";
+import { createClient } from "@/utils/supabase/server"; // supabase 객체 불러오기. // 서버 컴포넌트니깐 서버에서 임포트해온다.
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const supabase = createClient(); // supabase 객체 불러오기.
+
+  // const session = await supabase.auth.getSession();
+  // console.log(session.data.session?.user);
+
+  const user = (await supabase.auth.getUser()).data.user;
+  // console.log("user: ", user);
+
   return (
-    <div className="max-w-3xl mx-auto md:py-10 h-screen">
-      <div className="h-full border rounded-md">
-        <ChatHeader />
+    <>
+      <div className="max-w-3xl mx-auto md:py-10 h-screen">
+        <div className="h-full border rounded-md">
+          <ChatHeader user={user} />
+          {/* 이제 zustand를 통해 user의 state management를 하기 때문에 ChatHeader에 user props를 안넘겨줘도 되지만, 굳이 지울 필요도 없으므로 남겨준다. */}
+        </div>
       </div>
-    </div>
+      <InitUser user={user} />
+      {/* user state 관리를 시작할 수 있도록. */}
+    </>
   );
 };
 
--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -3,8 +3,13 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation"; // 주의할 점. next/navigation에서 임포트한다.
 
-const ChatHeader = () => {
+const ChatHeader = ({ user }: { user: User | null }) => {
+  const router = useRouter();
+
+  // props로 UserResponse를 받는다.
   const handleLoginWithGithub = () => {
     const supabase = createClient();
     supabase.auth.signInWithOAuth({
@@ -13,6 +18,13 @@ const ChatHeader = () => {
         redirectTo: location.origin + "/auth/callback",
       },
     });
+  };
+
+  const handleLogout = async () => {
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    // const { error } = await supabase.auth.signOut();
+    router.refresh();
   };
 
   return (
@@ -25,7 +37,11 @@ const ChatHeader = () => {
             <h1 className="text-sm text-gray-400">2 Online</h1>
           </div>
         </div>
-        <Button onClick={handleLoginWithGithub}>Login</Button>
+        {user ? ( // user가 있으면 (로그인했으면) 로그아웃 버튼 표시, 없으면 (로그인 안했으면) 로그인 버튼 표시
+          <Button onClick={handleLogout}>Logout</Button>
+        ) : (
+          <Button onClick={handleLoginWithGithub}>Login</Button>
+        )}
       </div>
     </div>
   );
